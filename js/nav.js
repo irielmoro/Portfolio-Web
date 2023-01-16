9f8b014fe6e5f08515994dc38a15cf6e02af3a13
Iriel moro <abrilmoro15@gmail.com>
2023-01-16 18:58:06 -0300

    rediseño de portfolio completo

--- js/nav.js
+++ js/nav.js
@@ -1,12 +1,14 @@
 const nav = document.querySelector('nav');
+const links = document.querySelectorAll('nav a');
 const logo = document.querySelector('.logo');
-
+console.log(links);
 let positionY= window.scrollY;
 
 window.addEventListener('scroll', ()=>{
 
     if ( positionY > window.scrollY) {
         nav.classList.remove('off');
+        links.forEach( e=> e.classList.remove('white'));
 
     } else {
         nav.classList.add('off');
@@ -15,6 +17,7 @@ window.addEventListener('scroll', ()=>{
     if ( window.scrollY > 60 ) {
         nav.classList.add('color');
         logo.classList.add('show');
+        links.forEach( e=> e.classList.add('white'));
         
     } else {
         nav.classList.remove('color');
